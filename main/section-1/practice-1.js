6094cd1fae8ff13c9a3e3cb6faf093e724934fbe
Tommy Hui <aesops0124@gmail.com>
2018-11-21 12:05:04 +0800

    [Tommy] Updated Practice 1

--- main/section-1/practice-1.js
+++ main/section-1/practice-1.js
@@ -1,16 +1,5 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  const collectionResult = [];
-  var k = 0;
-    for (var i = 0; i < collectionA.length; i++) {
-      for (var j = 0; j < collectionB.length; j++) {
-        if(collectionA[i] == collectionB[j]) {
-          collectionResult[k] = collectionA[i];
-          k++;
-        }
-      }
-    }
-    console.log(collectionResult);
-    return collectionResult;
+  return collectionA.filter(element => collectionB.includes(element))
 }
--- main/section-1/practice-2.js
+++ main/section-1/practice-2.js
@@ -1,18 +1,5 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  const collectionResult = [];
-  var m = 0;
-    for (var i = 0; i < collectionA.length; i++) {
-      for (var j = 0; j < collectionB.length; j++) {
-        for (var k = 0; k < collectionB[j].length; k++){
-          if(collectionA[i] == collectionB[j][k]) {
-            collectionResult[m] = collectionA[i];
-            m++;
-          }
-        }
-      }
-    }
-    console.log(collectionResult);
-    return collectionResult;
+   return collectionA.filter(element => [].concat.apply([], collectionB).includes(element))
 }
